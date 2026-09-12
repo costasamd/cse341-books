1e62b174ed7b8de6647eaf13b0d3eb7e8f4e33ad
costasamd <costasamd@icloud.com>
2026-09-12 07:45:58 +0100

    swagger file created

--- src/router.js
+++ src/router.js
@@ -3,6 +3,41 @@ import { getBooksHandler, getBookByIdHandler } from './controller/books.js';
 
 const router = express.Router();
 
+
+/**
+ * @swagger
+ * /books:
+ *   get:
+ *     summary: Get a list of books
+ *     tags: [Books]
+ *     responses:
+ *       200:
+ *         description: A list of books returned successfully
+ *       500:
+ *         description: Internal server error
+ */
 router.get('/books', getBooksHandler);
+
+/**
+ * @swagger
+ * /books/{id}:
+ *   get:
+ *     summary: Get a single book by ID
+ *     tags: [Books]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the book to retrieve
+ *     responses:
+ *       200:
+ *         description: The requested book
+ *       404:
+ *         description: Book not found
+ *       500:
+ *         description: Internal server error
+ */
 router.get('/books/:id', getBookByIdHandler);
 export default router;
